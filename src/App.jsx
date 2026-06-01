bb07aa07ee8aae1e169dce7205f7216b7965d379
Your New Name <vipuldhiman24@gmail.com>
2026-06-01 20:35:46 +0530

    quick update

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,43 @@
 import {
   Routes,
   Route,
-  useLocation,
 } from "react-router-dom";
 
-import { useEffect } from "react";
-
 import Home from "./pages/Home";
+
 import ProductDetails from "./pages/ProductDetails";
+
 import CartPage from "./pages/CartPage";
 
+import AdobeTracker from "./services/AdobeTracker";
+
 export default function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (
-      window.adobe?.target?.triggerView
-    ) {
-      window.adobe.target.triggerView(
-        location.pathname
-      );
-
-      console.log(
-        "Adobe Target View Triggered:",
-        location.pathname
-      );
-    }
-  }, [location.pathname]);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
 
-      <Route
-        path="/product/:id"
-        element={<ProductDetails />}
-      />
+    <>
 
-      <Route
-        path="/cart"
-        element={<CartPage />}
-      />
-    </Routes>
+      <AdobeTracker />
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/product/:id"
+          element={<ProductDetails />}
+        />
+
+        <Route
+          path="/cart"
+          element={<CartPage />}
+        />
+
+      </Routes>
+
+    </>
   );
 }
